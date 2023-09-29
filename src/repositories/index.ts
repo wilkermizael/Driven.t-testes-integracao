4c50ca133304c752ea02f9c72f53b34ff561d95d
Wilker Mizael <wilkermizael@Wilker-Macbook-Pro.local>
2023-09-29 20:44:01 -0300

    first commit

---
+++ src/repositories/index.ts
@@ -0,0 +1,7 @@
+export * from './address-repository';
+export * from './authentication-repository';
+export * from './enrollments-repository';
+export * from './events-repository';
+export * from './users-repository';
+export * from './tickets-repository';
+export * from './payments-repository';
